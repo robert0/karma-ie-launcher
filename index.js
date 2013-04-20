1e9598017d3690d8cf2e99dec0c1ceef3b7422fa
Roberto Amici <roberto.amici@gmail.com>
2013-04-20 10:35:56 +1000

    Adds -noframemerging command line option for IE Avoids issues with IE instance pooling that stop karma from detecting that the browser has started properly. Info here: http://msdn.microsoft.com/en-us/library/hh826025(v=vs.85).aspx

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var IEBrowser = function(baseBrowserDecorator) {
   baseBrowserDecorator(this);
+
+   this._getOptions = function(url) {
+    // Internet Explorer CLI options
+    // http://msdn.microsoft.com/en-us/library/hh826025(v=vs.85).aspx
+    return ['-noframemerging', url];
+  };
 };
 
 IEBrowser.prototype = {
